6f43b87344ab48ffde174c8f74c05e4505600032
ayanami <l2371792777@163.com>
2022-07-27 18:46:39 +0800

    blog

--- src/routes/view/account.js
+++ src/routes/view/account.js
@@ -5,15 +5,11 @@
 const router = require('koa-router')()
 const { loginRedirect } = require('../../middleware/loginCheck')
 
-//后续加入登录机制
 router.get('/',loginRedirect, async (ctx,next) => {
     await ctx.render('index')
 })
 router.get('/new',loginRedirect, async (ctx,next) => {
     await ctx.render('new')
-})
-router.get('/delete',loginRedirect, async (ctx,next) => {
-    await ctx.render('delete')
 })
 router.get('/alter',loginRedirect, async (ctx,next) => {
     await ctx.render('alter')
