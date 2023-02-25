70fde85736c51c63bda9217d758115fd6cda2ead
debian_ayanami <2371792777@qq.com>
2023-02-25 20:17:05 +0800

    conf mes

---
+++ src/conf-example/db.js
@@ -0,0 +1,19 @@
+/**
+ *  * @description 数据库信息
+ *   */
+
+const MYSQL_CONF={
+	    host:'119.29.37.150',
+	    user:'ayanami',
+	    password:'ayanami',
+	    database:'myaccount' 
+}
+const REDIS_CONF={
+	    host:'localhost',
+	    port:'6379'
+}
+
+module.exports={
+	    MYSQL_CONF,
+	    REDIS_CONF
+}
