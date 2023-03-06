3966cd6e9b132f5dd763a6e7426bdf160e23bba9
ayanami <l2371792777@163.com>
2023-03-06 16:05:50 +0800

    icon

---
+++ src/public/js/index.js
@@ -0,0 +1,4 @@
+let bodyselector = document.getElementsByTagName('body');
+bodyselector[0].style.background = "url('/img/background2.webp') no-repeat fixed center center"
+bodyselector[0].style.backgroundSize = "cover"
+
